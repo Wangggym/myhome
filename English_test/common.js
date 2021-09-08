a1a916e4b0f989a2c20560ecc02e81796a93fc91
wangym <wangym0203@aliyun.com>
2021-09-08 16:09:25 +0800

    feat: update common.js

--- English_test/common.js
+++ English_test/common.js
@@ -1,4 +1,4 @@
-const clears = document.getElementsByClassName("LineHeight");
+const clears = document.getElementsByClassName("clear");
 for (let i = 0; i < clears.length; i++) {
-  clears[i].innerHTML = '<input style="width:600px"/>';
+  clears[i].innerHTML = '<input style="width:600px; margin-left: 50px;"/>';
 }
